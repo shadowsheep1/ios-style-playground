a7a1ba4ffdd8ec711e689ca46928fdb49af9d051
shadowsheep <16268789+shadowsheep1@users.noreply.github.com>
2020-04-29 10:34:14 +0200

    input view programmatically added

--- Style Playground/ViewController.swift
+++ Style Playground/ViewController.swift
@@ -9,9 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var keyboardToolbar: UIToolbar!
+    
     @IBOutlet weak var myTextField: UITextField!
+    
+    let accessory: UIView = {
+        let accessoryView = UIView(frame: .zero)
+        accessoryView.backgroundColor = .lightGray
+        accessoryView.alpha = 0.6
+        return accessoryView
+    }()
+    
+    let doneButton: UIButton = {
+        let doneButton = UIButton(type: .custom)
+        doneButton.setTitle("Done", for: .normal)
+        doneButton.setTitleColor(StyleManager.colorBlue(theme: StyleManager.getTheme()), for: .normal)
+        doneButton.addTarget(self, action:#selector(doneButtonTapped), for: .touchUpInside)
+        doneButton.showsTouchWhenHighlighted = true
+        return doneButton
+    }()
+    
+    func addAccessory() {
+        accessory.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 45)
+        accessory.translatesAutoresizingMaskIntoConstraints = false
+        doneButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        accessory.addSubview(doneButton)
+        
+        self.myTextField.inputAccessoryView = accessory
+        
+        NSLayoutConstraint.activate([
+            doneButton.trailingAnchor.constraint(equalTo:accessory.trailingAnchor, constant: -20),
+            doneButton.centerYAnchor.constraint(equalTo:accessory.centerYAnchor),
+        ])
+    }
+    
+    @objc func doneButtonTapped() {
+        self.myTextField.resignFirstResponder()
+    }
     
     @IBAction func toolbarDoneClick(_ sender: Any) {
         self.myTextField.resignFirstResponder()
@@ -21,9 +55,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        self.myTextField.inputAccessoryView = self.keyboardToolbar
+        self.addAccessory()
     }
-
+    
     @IBAction func toggleTheme(_ sender: Any) {
         if StyleManager.getTheme() == StyleManager.themeDefault {
             StyleManager.setTheme(theme: StyleManager.themeVariant1, viewController: self)
